40f8166e8c9ff36143cbd6c43fb964dfbc88c4f6
caioslopes <caiolopes.social@gmail.com>
2024-06-13 08:48:41 -0300

    feat: adding shadcn config

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <span>Apollo Music</span>
+      <span>Login</span>
     </main>
   );
 }
