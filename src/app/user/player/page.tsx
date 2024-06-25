68fde75985205a61518a90c1cc195ba55f41bbb4
caioslopes <caiolopes.social@gmail.com>
2024-06-24 23:29:36 -0300

    feat: adding other user pages, and refactor layout of user

---
+++ src/app/user/player/page.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function player() {
+  return <div>player</div>;
+}
--- src/views/User/User.tsx
+++ src/views/User/User.tsx
@@ -1,9 +1,28 @@
+"use client";
+import { deleteAccessTokenFromCookies } from "@/auth/deleteAccessTokenFromCookies";
+import { Button } from "@/components/ui/button";
+import { LogOut } from "lucide-react";
+import { useRouter } from "next/navigation";
 import React from "react";
 
 export default function User() {
+  const router = useRouter();
   return (
-    <div className="flex justify-center items-center min-h-screen bg-primary">
-      <span className="text-white font-bold text-3xl">User home</span>
+    <div>
+      <p className="font-bold">User home</p>
+      <div className="flex justify-between">
+        Sair
+        <Button
+          className="p-0"
+          variant="ghost"
+          onClick={async () => {
+            await deleteAccessTokenFromCookies();
+            router.push("/");
+          }}
+        >
+          <LogOut className="text-primary" size={18} />
+        </Button>
+      </div>
     </div>
   );
 }
